af85dbb3beaf3211ec726821c122b5a88848ad10
samuelmoosmann <me@samuelmoosmann.at>
2017-06-21 18:46:32 +0200

    Initial Commit

---
+++ Stocker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stocker
+//
+//  Created by Samuel Moosmann on 21/06/2017.
+//  Copyright © 2017 Samuel Moosmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
